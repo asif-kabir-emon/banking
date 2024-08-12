979df1c54a9a4e5c4083cbeefe3ccbc002d10310
MD ASIF KABIR EMON <79392749+asif-kabir-emon@users.noreply.github.com>
2024-08-12 15:08:16 +0600

    Implement Auth, AppWrite, and Sentry

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
+import Footer from "./Footer";
 
 const Sidebar = ({ user }: SidebarProps) => {
     const pathName = usePathname();
@@ -61,7 +62,7 @@ const Sidebar = ({ user }: SidebarProps) => {
                 })}
                 USER
             </nav>
-            Footer
+            <Footer user={user} type="desktop" />
         </section>
     );
 };
---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://3710c7ec56c91dfd188c183511963428@o4507685784125440.ingest.us.sentry.io/4507763507593216",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
